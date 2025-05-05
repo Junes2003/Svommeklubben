44ccd9289a6072d5eb2951a0e3a1b244cf8bb21a
aljamour <alal0002@stud.net.dk>
2025-05-05 10:23:01 +0200

    d

--- src/Discipline.java
+++ src/Discipline.java
@@ -1,3 +1,12 @@
 public enum Discipline {
-    DISCIPLINE1, DISCIPLINE2, DISCIPLINE3, DISCIPLINE4
+    DISCIPLINE1,
+    DISCIPLINE2,
+    DISCIPLINE3,
+    DISCIPLINE4
+    /*
+    Butterfly
+    Crawl
+    Breaststroke
+    Backstroke
+     */
 }
