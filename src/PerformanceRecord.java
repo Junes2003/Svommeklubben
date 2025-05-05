324079b49f63207698e565f8a3c8f1f41ff6d0eb
aljamour <alal0002@stud.net.dk>
2025-05-05 11:34:21 +0200

    junes er

--- src/PerformanceRecord.java
+++ src/PerformanceRecord.java
@@ -1,6 +1,19 @@
 public class PerformanceRecord {
 
     private int memberID;
+    private int recordID;
+    private int disciplineID;
+    private String trainingBestDate;
+    private String trainingBestTime;
+
+    public PerformanceRecord(int memberID, int recordID, int disciplineID, String trainingBestDate, String trainingBestTime) {
+        this.memberID = memberID;
+        this.recordID = recordID;
+        this.disciplineID = disciplineID;
+        this.trainingBestDate = trainingBestDate;
+        this.trainingBestTime = trainingBestTime;
+    }
+
 
 
 
