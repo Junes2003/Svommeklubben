3c669118258b52923b211bcfa58dfa1696f79b43
aljamour <alal0002@stud.net.dk>
2025-05-05 12:13:16 +0200

    daded

--- src/Result.java
+++ src/Result.java
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-public class Result {
-=======
 public class Result extends PerformanceRecord {
 
     public Result(int memberID, int recordID, int disciplineID, String trainingBestDate, String trainingBestTime) {
         super(memberID, recordID, disciplineID, trainingBestDate, trainingBestTime);
     }
->>>>>>> be9f88490eb1831c8e5e9ea31cde44b960e03aab
 
 }
 
