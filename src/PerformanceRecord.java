34e282a3ebcff34a2f0d260a3dd039cd84e25b8e
moha4733 <moonyahidi@gmail.com>
2025-05-05 11:32:32 +0200

    added change

--- src/PerformanceRecord.java
+++ src/PerformanceRecord.java
@@ -1,22 +1,46 @@
+import java.time.LocalDateTime;
+
 public class PerformanceRecord {
 
     private int memberID;
+    private int recordID;
+    private int diciplineID;
+    private LocalDateTime trainingBestDate;
+    private String trainingBestTime;
 
+    public PerformanceRecord(int memberID , int recordID , int diciplineID , LocalDateTime trainingBestDate , String trainingBestTime ){
+        this.memberID=memberID;
+        this.diciplineID=diciplineID;
+        this.recordID=recordID;
+        this.trainingBestDate=trainingBestDate;
+        this.trainingBestTime=trainingBestTime;
 
+    }
+    public String getTrainingBestTime(){
+        return trainingBestTime;
+    }
 
-    //-trainingBestDate: LocalDate
-    //-disciplineID: Integer
-    //-memberID: Integer
-    //recordID: Integer
-    //-trainingBestTime: boolean
-    //+ getRecordID(): Integer
-    //+ getMemberID(): Integer
-    //+getDisciplineID(): Integer
-    //+ getTrainingBestTime(): boolean
-    //+ getTrainingBestDate(): LocalDate
+    public int getMemberID(){
+        return memberID;
+    }
 
+    public int getRecordID(){
+        return recordID;
+    }
 
+    public int getDiciplineID(){
+        return diciplineID;
 
+    }
 
+    public LocalDateTime getTrainingBestDate(){
+        return trainingBestDate;
+
+    }
+    @Override
+    public String toString() {
+        return "PerformanceRecord{" + "memberID=" + memberID + ", recordID=" + recordID + ", diciplineID=" + diciplineID + ", trainingBestDate=" + trainingBestDate + ", trainingBestTime='" + trainingBestTime + '\'' + '}';
+    }
 
 }
+
