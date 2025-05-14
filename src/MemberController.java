7e55d1fc0d17cd42d9449e0e6feb9740ea5230df
moha4733 <moonyahidi@gmail.com>
2025-05-14 10:38:11 +0200

    Added change

--- src/MemberController.java
+++ src/MemberController.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class MemberController {
+public class gMemberController {
 
     private List<Member> allMembers;
     private List<Member> activeMembers;
